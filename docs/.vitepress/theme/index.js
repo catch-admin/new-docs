c03ca42f1ec4bf1382b52affe9da833b7eab1a68
JaguarJack <njphper@gmail.com>
2024-01-20 16:59:08 +0800

    新增404

--- docs/.vitepress/theme/index.js
+++ docs/.vitepress/theme/index.js
@@ -3,13 +3,15 @@ import { h } from 'vue'
 import DefaultTheme from 'vitepress/theme'
 import Pro from './pro.vue'
 import Star from './star.vue'
+import NotFound from './notfound.vue'
 
 export default {
   extends: DefaultTheme,
   Layout() {
     return h(DefaultTheme.Layout, null, {
       'aside-outline-after': () => h(Pro),
-      'doc-top': () => h(Star)
+      'doc-top': () => h(Star),
+      'not-found': () => h(NotFound)
     })
   }
 }
