9ce361eda23d64164e6940db6aa9fc48206bb380
JaguarJack <njphper@gmail.com>
2024-01-21 09:56:23 +0800

    base修改

--- docs/.vitepress/config.mjs
+++ docs/.vitepress/config.mjs
@@ -10,7 +10,7 @@ const version3front = `${version3root}/front`
 export default defineConfig({
   title: 'CatchAdmin',
   description: 'CatchAdmin',
-  base: 'https://catchadmin.com/',
+  base: '/',
   assetsDir: 'docs/assets',
   cleanUrls: true,
   rewrites: {
