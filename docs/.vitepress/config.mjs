88868f5c58b134a1daf3facd313192f824d4830b
JaguarJack <njphper@gmail.com>
2024-01-28 17:39:27 +0800

    chore

--- docs/.vitepress/config.mjs
+++ docs/.vitepress/config.mjs
@@ -124,51 +124,8 @@ export default defineConfig({
       provider: 'algolia',
       options: {
         appId: 'Z82AVC6OQ9',
-        apiKey: '3418e43d03b752b8354c9fa5295e4547',
-        indexName: 'catchadmin',
-        locales: {
-          zh: {
-            placeholder: '搜索文档',
-            translations: {
-              button: {
-                buttonText: '搜索文档',
-                buttonAriaLabel: '搜索文档'
-              },
-              modal: {
-                searchBox: {
-                  resetButtonTitle: '清除查询条件',
-                  resetButtonAriaLabel: '清除查询条件',
-                  cancelButtonText: '取消',
-                  cancelButtonAriaLabel: '取消'
-                },
-                startScreen: {
-                  recentSearchesTitle: '搜索历史',
-                  noRecentSearchesText: '没有搜索历史',
-                  saveRecentSearchButtonTitle: '保存至搜索历史',
-                  removeRecentSearchButtonTitle: '从搜索历史中移除',
-                  favoriteSearchesTitle: '收藏',
-                  removeFavoriteSearchButtonTitle: '从收藏中移除'
-                },
-                errorScreen: {
-                  titleText: '无法获取结果',
-                  helpText: '你可能需要检查你的网络连接'
-                },
-                footer: {
-                  selectText: '选择',
-                  navigateText: '切换',
-                  closeText: '关闭',
-                  searchByText: '搜索提供者'
-                },
-                noResultsScreen: {
-                  noResultsText: '无法找到相关结果',
-                  suggestedQueryText: '你可以尝试查询',
-                  reportMissingResultsText: '你认为该查询应该有结果？',
-                  reportMissingResultsLinkText: '点击反馈'
-                }
-              }
-            }
-          }
-        }
+        apiKey: '5883216465758859a57ee9d7332d79c3',
+        indexName: 'catchadmin'
       }
     },
 
