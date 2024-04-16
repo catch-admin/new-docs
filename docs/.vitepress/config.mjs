13ec22b4a2291af1a282f305b5927602af8904de
JaguarJack <njphper@gmail.com>
2024-04-16 19:08:28 +0800

    chore:修改搜索

--- docs/.vitepress/config.mjs
+++ docs/.vitepress/config.mjs
@@ -183,6 +183,10 @@ export default defineConfig({
                 link: `${forms}/basic/upload`
               },
               {
+                text: 'Tree 组件',
+                link: `${forms}/basic/tree`
+              },
+              {
                 text: '链接组件',
                 link: `${forms}/basic/url`
               },
@@ -361,55 +365,7 @@ export default defineConfig({
     },
 
     search: {
-      provider: 'algolia',
-      options: {
-        appId: '1JRPGQ44JC',
-        apiKey: '150489741e0052b72a9dbfc4aca92518',
-        indexName: 'catchadmindocs',
-        locales: {
-          zh: {
-            placeholder: '搜索文档',
-            translations: {
-              button: {
-                buttonText: '搜索文档',
-                buttonAriaLabel: '搜索文档'
-              },
-              modal: {
-                searchBox: {
-                  resetButtonTitle: '清除查询条件',
-                  resetButtonAriaLabel: '清除查询条件',
-                  cancelButtonText: '取消',
-                  cancelButtonAriaLabel: '取消'
-                },
-                startScreen: {
-                  recentSearchesTitle: '搜索历史',
-                  noRecentSearchesText: '没有搜索历史',
-                  saveRecentSearchButtonTitle: '保存至搜索历史',
-                  removeRecentSearchButtonTitle: '从搜索历史中移除',
-                  favoriteSearchesTitle: '收藏',
-                  removeFavoriteSearchButtonTitle: '从收藏中移除'
-                },
-                errorScreen: {
-                  titleText: '无法获取结果',
-                  helpText: '你可能需要检查你的网络连接'
-                },
-                footer: {
-                  selectText: '选择',
-                  navigateText: '切换',
-                  closeText: '关闭',
-                  searchByText: '搜索提供者'
-                },
-                noResultsScreen: {
-                  noResultsText: '无法找到相关结果',
-                  suggestedQueryText: '你可以尝试查询',
-                  reportMissingResultsText: '你认为该查询应该有结果？',
-                  reportMissingResultsLinkText: '点击反馈'
-                }
-              }
-            }
-          }
-        }
-      }
+      provider: 'local'
     },
 
     returnToTopLabel: '回到顶部',
